d3e25930916bd0ac1d9768e905997bfea4aae2f5
Bohan Cheng <vipc.1224@live.ca>
2025-05-06 17:32:54 -0400

    Added basic UI and UI manager

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+
+public class UIManager : MonoBehaviour
+{
+  [Header("Refs")]
+  [SerializeField] private GameObject menuPanel;
+  [SerializeField] private Button menuButton;
+  [SerializeField] private Button toggleModeButton;
+  [SerializeField] private TMP_Text modeButtonText;
+  [SerializeField] private Button toggleMusicButton;
+  [SerializeField] private TMP_Text musicButtonText;
+  [SerializeField] private Button resumeButton;
+  [SerializeField] private Button exitButton;
+  [SerializeField] private TMP_Text scoreText;
+
+  private bool isPaused = false;
+  private bool isAIControlled = false;
+  private bool isMusicOn = true;
+
+  private void Awake()
+  {
+    menuButton.onClick.AddListener(OpenMenu);
+    toggleModeButton.onClick.AddListener(ToggleControlMode);
+    toggleMusicButton.onClick.AddListener(ToggleMusic);
+    resumeButton.onClick.AddListener(ResumeGame);
+    exitButton.onClick.AddListener(ExitGame);
+
+    menuPanel.SetActive(false);
+    menuButton.gameObject.SetActive(true);
+  }
+
+  private void OnEnable()
+  {
+    var playerInput = GetComponent<PlayerInput>();
+    if (playerInput != null)
+    {
+      playerInput.actions["Pause"].performed += OnPauseInput;
+    }
+  }
+
+  private void OnDisable()
+  {
+    var playerInput = GetComponent<PlayerInput>();
+    if (playerInput != null)
+    {
+      playerInput.actions["Pause"].performed -= OnPauseInput;
+    }
+  }
+
+  private void OnPauseInput(InputAction.CallbackContext context)
+  {
+    if (!isPaused)
+      OpenMenu();
+    else
+      ResumeGame();
+  }
+
+  public void OpenMenu()
+  {
+    isPaused = true;
+    Time.timeScale = 0f;
+    menuPanel.SetActive(true);
+    menuButton.gameObject.SetActive(false);
+  }
+
+  public void ResumeGame()
+  {
+    isPaused = false;
+    Time.timeScale = 1f;
+    menuPanel.SetActive(false);
+    menuButton.gameObject.SetActive(true);
+  }
+
+  public void ToggleControlMode()
+  {
+    isAIControlled = !isAIControlled;
+    Debug.Log("Mode switched to: " + (isAIControlled ? "AI" : "Player"));
+
+    modeButtonText.text = isAIControlled ? "Start Player Mode" : "Start AI Mode";
+  }
+
+  public void ToggleMusic()
+  {
+    isMusicOn = !isMusicOn;
+    Debug.Log("Music " + (isMusicOn ? "On" : "Off"));
+
+    AudioListener.volume = isMusicOn ? 1f : 0f;
+
+    musicButtonText.text = isMusicOn ? "Turn Music Off" : "Turn Music On";
+  }
+
+  public void ExitGame()
+  {
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+  }
+
+  public void UpdateScore(int score)
+  {
+    scoreText.text = "Score: " + score;
+  }
+}
